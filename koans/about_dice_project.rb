3c8f9e2d23f501d7a221b4d44b542b7b8ff7b1a2
Piotr Grabowski <grapox@gmail.com>
2011-10-23 16:36:53 +0200

    about_dice completed

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,18 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+
+class DiceSet
+    attr_reader :values
+    def roll(number)
+        raise StandardError, "Number must be positive" if number <= 0 
+        @values = []
+        (1..number).each do
+            @values << (rand(6) + 1)
+        end
+    end
+
+end
 
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
